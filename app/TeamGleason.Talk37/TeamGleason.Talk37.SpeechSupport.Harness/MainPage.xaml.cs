45ed41a0160265ae03e17f8c916fcdeb2f3854a9
Pete Ansell <me@peteansell.com>
2017-07-25 14:30:14 -0700

    Added some basic text-to-speech code.

---
+++ app/TeamGleason.Talk37/TeamGleason.Talk37.SpeechSupport.Harness/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TeamGleason.Talk37.SpeechSupport.Harness
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        readonly TextToSpeechEngine _engine = new TextToSpeechEngine();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        async void OnPlayClicked(object sender, RoutedEventArgs e)
+        {
+            var text = TheTextBox.Text;
+            TheTextBox.SelectAll();
+
+            var stream = await _engine.SayText(text);
+
+            foreach (var mark in stream.Markers)
+            {
+                Debug.WriteLine($"{mark.Text} @ {mark.Time.Milliseconds}ms");
+            }
+
+            TheMedia.SetSource(stream, stream.ContentType);
+            TheMedia.Play();
+        }
+    }
+}
---
+++ app/TeamGleason.Talk37/TeamGleason.Talk37.SpeechSupport/TextToSpeechEngine.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.SpeechSynthesis;
+
+namespace TeamGleason.Talk37.SpeechSupport
+{
+    /// <summary>
+    /// Text to speech logic.
+    /// </summary>
+    public class TextToSpeechEngine
+    {
+        readonly SpeechSynthesizer _synthesizer = new SpeechSynthesizer();
+
+        /// <summary>
+        /// Speak given text.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        public async Task<SpeechSynthesisStream> SayText(string text)
+        {
+            for (var index = 0; index < text.Length; index++)
+            {
+                Debug.WriteLineIf(char.IsHighSurrogate(text, index), $"{index} is high surrogate");
+                Debug.WriteLineIf(char.IsLowSurrogate(text, index), $"{index} is low surrogate");
+                Debug.WriteLineIf(char.IsSurrogatePair(text, index), $"{index} is surrogate pair");
+                Debug.WriteLineIf(char.IsSurrogate(text, index), $"{index} is surrogate");
+            }
+
+            var position = 0;
+            while (position < text.Length)
+            {
+                Debug.WriteLine($"{position} is {char.ConvertToUtf32(text, position)}");
+                position += char.IsSurrogatePair(text, position) ? 2 : 1;
+            }
+
+            var ssmlBuilder = new StringBuilder("<speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'>");
+            ssmlBuilder.Append("<mark name='Start'/>");
+            ssmlBuilder.Append(text);
+            ssmlBuilder.Append("<mark name='Finish'/>");
+            ssmlBuilder.Append("</speak>");
+
+            var ssml = ssmlBuilder.ToString();
+
+            var stream = await _synthesizer.SynthesizeSsmlToStreamAsync(ssml);
+
+            foreach (var mark in stream.Markers)
+            {
+                Debug.WriteLine($"{mark.Text} @ {mark.Time.Milliseconds}ms");
+            }
+
+            return stream;
+        }
+    }
+}
