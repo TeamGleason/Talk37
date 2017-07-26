5baf2b6c1641d3883300e87e3a34fb5875df0cd9
Emily Clemens <t-emclem@microsoft.com>
2017-07-25 19:55:29 -0700

    Fleshed out DeviceConnection

--- app/TeamGleason.Talk37/TeamGleason.Talk37.ComSupport/DeviceConnection.cs
+++ app/TeamGleason.Talk37/TeamGleason.Talk37.ComSupport/DeviceConnection.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Media;
+
+/*
+<Capabilities>
+    <DeviceCapability Name="serialcommunication">
+        <Device Id="any">
+            <Function Type="name:serialPort" />
+        </Device>
+    </DeviceCapability>
+</Capabilities>
+*/
 
 namespace TeamGleason.Talk37.ComSupport
 {
     public class DeviceConnection
     {
-        DeviceConnection()
+        public DeviceConnection(string comPort)
         {
-
+            _arduinoConnectivity = new ArduinoConnectivityUWP();
+            _comPort = comPort;
         }
+
+        public async void PlayAnimationSequence(List<IMediaMarker> markers)
+        {
+            await _arduinoConnectivity.Connect(_comPort);
+
+            foreach (var marker in markers)
+            {
+                await _arduinoConnectivity.UploadCompressedSequence(marker.Text);
+                await Task.Delay(marker.Time);
+            }
+
+            await _arduinoConnectivity.ClearSequence();
+        }
+
+        private ArduinoConnectivityUWP _arduinoConnectivity;
+        private string _comPort;
     }
 }
