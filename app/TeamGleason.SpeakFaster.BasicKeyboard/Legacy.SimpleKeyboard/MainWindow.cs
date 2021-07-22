cfbc98850599ecc6edcc9fc5dc04d81713cc3906
Pete Ansell <petea@microsoft.com>
2021-07-21 17:55:48 -0700

    Used NuGet GazePointer to enable crude gaze within application.

---
+++ app/TeamGleason.SpeakFaster.BasicKeyboard/Legacy.SimpleKeyboard/MainWindow.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.HandsFree.GazePointer;
+using System;
+using System.Windows;
+using System.Windows.Interop;
+using TeamGleason.SpeakFaster.BasicKeyboard.Special;
+using TeamGleason.SpeakFaster.KeyboardLayouts;
+
+namespace TeamGleason.SpeakFaster.SimpleKeyboard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var layout = KeyboardLayout.ReadDefaultKeyboardLayout();
+            TheKeyboard.Layout = layout;
+
+            Loaded += (s, e) => GazePointer.Attach(this);
+            Closed += (s, e) => GazePointer.DetachAll();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            //Set the window style to noactivate.
+            var helper = new WindowInteropHelper(this);
+            InteropHelper.SetMainWindowStyle(helper.Handle);
+        }
+    }
+}
