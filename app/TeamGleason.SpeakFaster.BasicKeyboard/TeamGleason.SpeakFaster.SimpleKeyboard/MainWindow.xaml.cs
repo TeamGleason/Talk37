495ae694f775b4e37f3c039bec85e0ee17b2dccf
Pete Ansell <petea@microsoft.com>
2021-07-22 14:53:27 -0700

    Added persistence of window position between close and restart.

--- app/TeamGleason.SpeakFaster.BasicKeyboard/TeamGleason.SpeakFaster.SimpleKeyboard/MainWindow.xaml.cs
+++ app/TeamGleason.SpeakFaster.BasicKeyboard/TeamGleason.SpeakFaster.SimpleKeyboard/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Interop;
 using TeamGleason.SpeakFaster.BasicKeyboard.Special;
 using TeamGleason.SpeakFaster.KeyboardLayouts;
+using TeamGleason.SpeakFaster.SimpleKeyboard.Properties;
 
 namespace TeamGleason.SpeakFaster.SimpleKeyboard
 {
@@ -11,12 +13,53 @@ namespace TeamGleason.SpeakFaster.SimpleKeyboard
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static bool TryParseRect(string source, out Rect rect)
+        {
+            bool value;
+
+            try
+            {
+                rect = Rect.Parse(source);
+                value = true;
+            }
+            catch (InvalidOperationException)
+            {
+                value = false;
+            }
+
+            return value;
+        }
+
+        private bool SetWindowPosition(string rectString)
+        {
+            var value = TryParseRect(rectString, out var rect);
+            if (value)
+            {
+                Left = rect.Left;
+                Top = rect.Top;
+                Width = rect.Width;
+                Height = rect.Height;
+            }
+            return value;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
 
+            SetWindowPosition(Settings.Default.WindowRect);
+
             var layout = KeyboardLayout.ReadDefaultKeyboardLayout();
             TheKeyboard.Layout = layout;
+
+            Closing += OnClosing;
+        }
+
+        private void OnClosing(object sender, CancelEventArgs e)
+        {
+            var rect = new Rect(x: Left, y: Top, width: Width, height: Height);
+            Settings.Default.WindowRect = rect.ToString();
+            Settings.Default.Save();
         }
 
         protected override void OnSourceInitialized(EventArgs e)
