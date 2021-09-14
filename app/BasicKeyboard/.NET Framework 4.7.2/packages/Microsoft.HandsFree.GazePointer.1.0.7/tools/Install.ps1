aef4414f84c75c011da38a80c897330ec09acab1
Pete Ansell <petea@microsoft.com>
2021-09-13 21:07:36 -0700

    Imported eye gaze support.

---
+++ app/BasicKeyboard/.NET Framework 4.7.2/packages/Microsoft.HandsFree.GazePointer.1.0.7/tools/Install.ps1
@@ -0,0 +1,12 @@
+param($installPath, $toolsPath, $package, $project)
+
+$dllFiles = "Tobii.EyeX.Client.dll", "TobiiGazeCore64.dll"
+
+foreach ($dllFile in $dllFiles)
+{
+  $file1 = $project.ProjectItems.Item($dllFile)
+  
+  # set 'Copy To Output Directory' to 'Copy always'
+  $copyToOutput1 = $file1.Properties.Item("CopyToOutputDirectory")
+  $copyToOutput1.Value = 1
+}
